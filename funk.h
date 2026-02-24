30ca9e10fa4cf762fec9c705720d4e1b63e240e9
anna-omelchiuk <anna.omelchiuk@gmail.com>
2026-02-24 21:07:15 +0200

    Update function declarations and comments in funk.h

--- funk.h
+++ funk.h
@@ -3,33 +3,33 @@
 
 #include <iostream>
 
-// Завдання 1
+// Р—Р°РІРґР°РЅРЅСЏ 1
 
-int* AllocateArr(int size);   // Прототип розподілу динамічної пам'яті.
+int* AllocateArr(int size);
 
-void InitArr(int* arr, int size);  // Прототип ініціалізації динамічного масиву.
+void InitArr(int* arr, int size);
 
-void PrintArr(const int* arr, int size);  // Прототип друку динамічного масиву.
+void PrintArr(const int* arr, int size);
 
-void DelArr(int* arr);  // Прототип видалення динамічного масиву.
+void DelArr(int* arr);
 
-void AddElem(int*& arr, int& size, int value);  // Прототип додавання елемента в кінець масиву.
+void AddElem(int*& arr, int& size, int value);
 
-void InsertElem(int*& arr, int& size, int index, int value); // Прототип вставки елемента за вказаним індексом.
+void InsertElem(int*& arr, int& size, int index, int value);
 
-void RemoveElem(int*& arr, int& size, int index);  // Прототип видалення елемента за вказаним індексом.
+void RemoveElem(int*& arr, int& size, int index);
 
-// Завдання 2
+// Р—Р°РІРґР°РЅРЅСЏ 2
 
 
-int* RemovePrimes(const int* arr, int size, int& newSize);  // Прототип, який отримує покажчик на динамічний масив і його розмір.
+int* RemovePrimes(const int* arr, int size, int& newSize);
 
-// Завдання 3
+// Р—Р°РІРґР°РЅРЅСЏ 3
 
 
 void SplitBySign(const int* arr, int size,  
     int*& positives, int& posSize,
     int*& negatives, int& negSize,
-    int*& zeros, int& zeroSize);    // Прототип, який отримує покажчик на статичний масив і його розмір. 
+    int*& zeros, int& zeroSize);  
 
 #endif // FUNK_H
